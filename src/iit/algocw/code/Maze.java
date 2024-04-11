2c66da2f938f9422ea0273b0cbf566c6ebcb117e
Sevinda Perera <sevindacharukaperera@gmail.com>
2024-04-12 01:27:52 +0530

    Maze class implemented. (Find neighbors function created)

--- src/iit/algocw/code/Maze.java
+++ src/iit/algocw/code/Maze.java
@@ -1,15 +1,84 @@
 package iit.algocw.code;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * <h1>Maze</h1>
- * <p></p>
+ * <p>rows - number of rows of the maze</p>
+ * <p>cols - number of columns of the maze</p>
+ * <p>nodes - each element in the maze is a node</p>
  */
 public class Maze {
     private int rows;
     private int cols;
     private Node[][] nodes;
 
+    /**
+     * <h2>Maze Constructor</h2>
+     * @param mazeData the data of the maze in the format of a 2d array
+     */
     public Maze(char[][] mazeData) {
         rows = mazeData.length;
+        cols = mazeData[0].length;
+        nodes = new Node[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                nodes[i][j] = new Node(i, j, mazeData[i][j] == '0');
+            }
+        }
+    }
+
+    /**
+     * <h3>getNeighborNodes function</h3>
+     * <p>Neighbors are nodes where it is adjacent to a wall or it is the last node in the maze</p>
+     * @param currentNode the current node where the pointer is.
+     * @return the list of neighbors the "current node" has.
+     */
+    public List<Node> getNeighborNodes(Node currentNode) {
+        List<Node> neighbors = new ArrayList<>();
+
+        int[] xDirection = {0, 0, -1, 1};
+        int[] yDirection = {-1, 1, 0, 0};
+
+        for (int i = 0; i < 4; i++) {
+            int newNodeRow = currentNode.getRow();
+            int newNodeCol = currentNode.getCol();
+
+            while ((newNodeRow >= 0 && newNodeRow < nodes.length) && (newNodeCol >= 0 && newNodeCol < nodes[0].length) && !nodes[newNodeRow][newNodeCol].isWall()) {
+                newNodeRow += xDirection[i];
+                newNodeCol += yDirection[i];
+            }
+
+            if (newNodeRow != currentNode.getRow() || newNodeCol != currentNode.getCol())
+                neighbors.add(nodes[newNodeRow][newNodeCol]);
+        }
+
+        return neighbors;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public int getCols() {
+        return cols;
+    }
+
+    public void setCols(int cols) {
+        this.cols = cols;
+    }
+
+    public Node[][] getNodes() {
+        return nodes;
+    }
+
+    public void setNodes(Node[][] nodes) {
+        this.nodes = nodes;
     }
 }
--- src/iit/algocw/testcode/Maze.java
+++ src/iit/algocw/testcode/Maze.java
@@ -34,7 +34,6 @@ public class Maze {
                 newCol += dy[i];
             }
         }
-
         return neighbors;
     }
 
